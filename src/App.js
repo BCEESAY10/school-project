8a0be5393eae0297c5dbaf144969257366b31c84
BCEESAY10 <bamfaceesay30@gmail.com>
2024-12-18 16:09:29 +0000

    Commiting codes for the login UI

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Form from "./components/Form";
+function App() {
+  return (
+    <div className="flex w-full h-screen">
+        <div className="w-full flex items-center justify-center lg:w-1/2">
+           <Form />
+        </div>
+        <div className="hidden relative lg:flex h-full bg-gray-200 w-1/2 items-center justify-center">
+            <div className="w-60 h-60 bg-gradient-to-tr from-violet-500 to-pink-500 rounded-full animate-bounce" /> 
+            <div className="w-full h-1/2 absolute bg-white/10 bottom-0 backdrop-blur-lg" />
+        </div>
+    </div>
+  );
+}
+
+export default App;
