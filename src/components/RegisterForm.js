64ae5621dc1e20379b1bd56b8cce95cdb7187224
BCEESAY10 <bamfaceesay30@gmail.com>
2024-12-23 13:22:11 +0000

    Fix responsiveness for Login and Register components

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -1,65 +1,112 @@
 import * as React from "react";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 export default function RegisterForm() {
+  const initialValues = {fname: "", lname: "", email: "", password: ""};
+  const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setValues({...values, [name]: value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setErrors(validate(values))
+    setIsSubmit(true)
+  }
+
+  useEffect(() => {
+    console.log(errors)
+    if(Object.keys(errors).length === 0 && isSubmit)
+      console.log(values)
+  }, [errors])
+
+  const validate = (someValues) => {
+    const formErrors = {}
+    const regex = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/i;
+    if(!someValues.fname){
+      formErrors.fname = "Please enter your first name";
+    }
+    if(!someValues.lname){
+      formErrors.lname = "Please enter your last name";
+    }
+    if(!someValues.email){
+      formErrors.email = "Email is required";
+    }
+    if(!someValues.password){
+      formErrors.password = "Please enter valid password";
+    }
+    return formErrors;
+  }
+
   return (
     <div className="bg-white px-10 py-20 rounded-3xl border-gray-200">
-      <h1 className="text-5xl font-semibold">Welcome to GS Schools</h1>
+      <h1 className="text-2xl md:text-5xl font-semibold">Welcome to GS Schools</h1>
       <h3 className="font-medium text-lg text-gray-500 mt-4">
         Please enter your details
       </h3>
 
-      <div className="mt-8">
+      <form onSubmit={handleSubmit} className="mt-8">
         <div>
           <label className="text-lg font-medium">First Name</label>
           <input
-            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+            className="w-full border-2 border-gray-100 rounded-xl p-3 md:p-4 mt-1 bg-transparent"
             placeholder="Enter your first name, and middle name if available"
+            value={values.fname}
+            onChange={handleChange}
+            name="fname"
             type="text"
           />
         </div>
         <div>
           <label className="text-lg font-medium">Last Name</label>
           <input
-            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+            className="w-full border-2 border-gray-100 rounded-xl p-3 md:p-4 mt-1 bg-transparent"
             placeholder="Enter your last name"
+            value={values.lname}
+            onChange={handleChange}
+            name="lname"
             type="text"
           />
         </div>
         <div>
           <label className="text-lg font-medium">Email</label>
           <input
-            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+            className="w-full border-2 border-gray-100 rounded-xl p-3 md:p-4 mt-1 bg-transparent"
             placeholder="Enter your email"
+            value={values.email}
+            onChange={handleChange}
+            name="email"
             type="email"
           />
         </div>
         <div>
           <label className="text-lg font-medium">Password</label> 
           <input
-            className="w-full border-2 border-gray-100 rounded-xl p-4 mt-1 bg-transparent"
+            className="w-full border-2 border-gray-100 rounded-xl p-3 md:p-4 mt-1 bg-transparent"
             placeholder="Enter your password"
+            value={values.password}
+            onChange={handleChange}
+            name="password"
             type="password"
           />
         </div>
         
         
         <div className="mt-8 flex flex-col gap-y-4">
-          <button className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">
+          <button type="submit" className="active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl bg-violet-500 text-white text-lg font-bold">
             Sign up
-          </button>
-          <button className="flex py-3 border-2 border-gray-200 active:scale-[.98] active:duration-75 hover:scale-[1.01] ease-in-out transition-all py-3 rounded-xl items-center justify-center gap-2">
-          <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 48 48">
-            <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path><path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path><path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"></path><path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
-          </svg>
-            Sign in with Google
           </button>
         </div>
         <div className="mt-4 flex gap-4">
            <h3>Already registered? </h3>
            <Link className="hover:underline" to="/">Login here</Link>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
